35f36c6f6b978a7345fb05c68d6fc37353825003
Chris Waalberg <chris@q42.nl>
2015-03-16 19:53:49 +0100

    Moved Calculator to subfolder and added Happiness

---
+++ Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Chris Waalberg on 16/02/15.
+//  Copyright (c) 2015 Q42. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+    
+
+}
